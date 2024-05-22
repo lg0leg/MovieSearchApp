754461690acbcbfa850b7e1717578f17b38d2305
lg0leg <lg.oleg1067@gmail.com>
2024-05-22 21:40:23 +0300

    feat: add 404 page

--- movie-search-app/src/pages/404/404.js
+++ movie-search-app/src/pages/404/404.js
@@ -1,5 +1,23 @@
+import { Button, Center, Flex, Space, Text } from '@mantine/core';
 import React from 'react';
+import notfound from '../../assets/png/404.png';
+import { NavLink } from 'react-router-dom';
 
 export default function NotFoundPage() {
-  return <div>404</div>;
+  return (
+    <Center h="100vh">
+      <Flex gap="16" direction="column" align="center">
+        <img src={notfound} alt="404" />
+        <Space h="32" />
+        <Text size="xl" fw="600">
+          We can’t find the page you are looking for
+        </Text>
+        <NavLink to="/movies" className="nav-item">
+          <Button variant="filled" color="#9854f6">
+            Go home
+          </Button>
+        </NavLink>
+      </Flex>
+    </Center>
+  );
 }
