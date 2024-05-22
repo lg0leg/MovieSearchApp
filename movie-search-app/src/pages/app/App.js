0237f042e4819a0fbda04bee3e5eb7780d8b0452
lg0leg <lg.oleg1067@gmail.com>
2024-05-22 15:21:22 +0300

    feat: add navbar

--- movie-search-app/src/pages/app/App.js
+++ movie-search-app/src/pages/app/App.js
@@ -4,31 +4,38 @@ import logo from '../../assets/svg/logo.svg';
 import NotFoundPage from '../404/404';
 import RatedMovies from '../rated-movies/rated-movies';
 import Movies from '../movies/movies';
+import { AppShell, Flex, Space, Title } from '@mantine/core';
 
 function App() {
   return (
-    <div className="app">
-      <div className="sidebar">
-        <div className="logo">
-          <img src={logo} alt="logo"></img>
-          <p className="title">ArrowFlicks</p>
-        </div>
-
-        <div className="app-nav">
+    <AppShell navbar={{ width: 280 }} main={{ width: 1160 }} withBorder={false}>
+      <AppShell.Navbar style={{ padding: '24px', backgroundColor: '#f2ecfa' }}>
+        <NavLink to="/movies" style={{ textDecoration: 'none' }}>
+          <Flex gap={12}>
+            <img src={logo} alt="logo"></img>
+            <Title order={2} className="title">
+              ArrowFlicks
+            </Title>
+          </Flex>
+        </NavLink>
+        <Space h="80" />
+        <Flex gap={16} direction="column">
           <NavLink to="/movies" className="nav-item">
             Movies
           </NavLink>
           <NavLink to="/rated-movies" className="nav-item">
             Rated movies
           </NavLink>
-        </div>
-      </div>
-      <Routes>
-        <Route path="movies" element={<Movies />} />
-        <Route path="rated-movies" element={<RatedMovies />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+        </Flex>
+      </AppShell.Navbar>
+      <AppShell.Main style={{ backgroundColor: '#f5f5f6' }}>
+        <Routes>
+          <Route path="movies" element={<Movies />} />
+          <Route path="rated-movies" element={<RatedMovies />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
