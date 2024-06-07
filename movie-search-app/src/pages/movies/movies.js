826422390d611dd317a6964f66a14dbf6aeeae69
lg0leg <lg.oleg1067@gmail.com>
2024-06-07 19:51:26 +0300

    feat: implement selectors functionality

--- movie-search-app/src/pages/movies/movies.js
+++ movie-search-app/src/pages/movies/movies.js
@@ -11,27 +11,60 @@ const options = {
 
 export default function Movies() {
   const [genresList, setGenresList] = useState([]);
-  const [genres, setGenres] = useState([]);
-  const [year, setYear] = useState('');
+  const [genres, setGenres] = useState('');
+  const [releaseYear, setReleaseYear] = useState('');
+  const [ratingFrom, setRatingFrom] = useState('');
+  const [ratingTo, setRatingTo] = useState('');
+
+  const [page, setPage] = useState('1');
+  const [sort, setSort] = useState('popularity.desc');
+
+  const [moviesInfo, setMoviesInfo] = useState([]);
+
+  const getMovies = async () => {
+    const baseURL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US';
+    let year = releaseYear ? `&primary_release_year=${releaseYear}` : '';
+    let rf = ratingFrom ? `&vote_average.gte=${ratingFrom}` : '';
+    let rt = ratingTo ? `&vote_average.lte=${ratingTo}` : '';
+    let gens = genres ? `&with_genres=${genresList.find((val) => val.name === genres).id}` : '';
+    let query = `${baseURL}&page=${page}${year}&sort_by=${sort}${rf}${rt}${gens}`;
+    let resp = await fetch(query, options);
+    let data = await resp.json();
+    setMoviesInfo(data.results);
+  };
 
   const getGenres = async () => {
     let resp = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options);
     let data = await resp.json();
-    let genresList = data.genres.map((val) => val.name);
-    setGenresList(genresList);
+    setGenresList(data.genres);
   };
 
   useEffect(() => {
     getGenres();
   }, []);
 
+  useEffect(() => {
+    getMovies();
+  }, [genres, releaseYear, ratingFrom, ratingTo]);
+
+  const years = Array.from({ length: 100 }, (_, index) => String(2024 - index));
+
   return (
     <Container style={{ paddingLeft: 90, paddingRight: 90, paddingTop: 40, margin: 0 }}>
       <Title order={1}>Movies</Title>
       <Flex gap="16">
-        <Select label="Genres" placeholder="Select genre" data={genresList} value={genres} onChange={setGenres} />
-        <Select label="Release year" placeholder="Select release year" data={['raz', 'dva']} value={year} onChange={setYear} />
+        <Select label="Genres" placeholder="Select genre" data={genresList.map((val) => val.name)} value={genres} onChange={setGenres} />
+        <Select label="Release year" placeholder="Select release year" data={years} value={releaseYear} onChange={setReleaseYear} />
+        <Select label="Ratings" placeholder="From" data={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']} value={ratingFrom} onChange={setRatingFrom} />
+        <Select label=" " placeholder="To" data={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']} value={ratingTo} onChange={setRatingTo} />
       </Flex>
+      <ul>
+        {moviesInfo.map((item, idx) => (
+          <li key={idx}>
+            <p>{item.original_title}</p>
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 }
