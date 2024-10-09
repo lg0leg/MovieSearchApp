0172104d0593ba9d967231d859e7849e6e6626a2
lg0leg <lg.oleg1067@gmail.com>
2024-10-09 21:30:37 +0300

    feat: add sort

--- movie-search-app/src/pages/movies/movies.js
+++ movie-search-app/src/pages/movies/movies.js
@@ -44,24 +44,48 @@ export default function Movies() {
     getGenres();
   }, []);
 
+  // useEffect(() => {
+  //   console.log(sort);
+  // }, [sort]);
+
   useEffect(() => {
     getMovies();
-  }, [genres, releaseYear, ratingFrom, ratingTo]);
+  }, [genres, releaseYear, ratingFrom, ratingTo, sort]);
 
   const years = Array.from({ length: 100 }, (_, index) => String(2024 - index));
 
   return (
-    <Container fluid style={{ paddingLeft: 90, paddingRight: 90, paddingTop: 40, margin: 0 }}>
+    <Container fluid style={{ width: 1160, paddingLeft: 90, paddingRight: 90, paddingTop: 40, margin: 0 }}>
       <Title order={1}>Movies</Title>
       <Space h="40" />
       <Flex gap="16" wrap="wrap" align="flex-end">
         <Select className="filter-title" label="Genres" placeholder="Select genre" data={genresList.map((val) => val.name)} value={genres} onChange={setGenres} />
         <Select className="filter-title" label="Release year" placeholder="Select release year" data={years} value={releaseYear} onChange={setReleaseYear} />
-        <Select className="filter-title" label="Ratings" placeholder="From" data={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']} value={ratingFrom} onChange={setRatingFrom} />
-        <Select className="filter-title" label=" " placeholder="To" data={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']} value={ratingTo} onChange={setRatingTo} />
+        <Select
+          className="filter-title filter-title-short"
+          label="Ratings"
+          placeholder="From"
+          data={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
+          value={ratingFrom}
+          onChange={setRatingFrom}
+        />
+        <Select className="filter-title filter-title-short" label=" " placeholder="To" data={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']} value={ratingTo} onChange={setRatingTo} />
         <Button pb={10} variant="transparent" color="rgb(123, 124, 136)">
           Reset filters
         </Button>
+      </Flex>
+      <Flex style={{ marginTop: 10 }}>
+        <Select
+          className="filter-title"
+          label="Sort by"
+          data={[
+            { value: 'popularity.desc', label: 'Most popular' },
+            { value: 'title.desc', label: 'Title' },
+            { value: 'vote_average.desc', label: 'Rating' },
+          ]}
+          value={sort}
+          onChange={(_value, option) => setSort(option.value)}
+        />
       </Flex>
       <ul>
         {moviesInfo.map((item, idx) => (
