d044b4247ae68ec89dc86982c0b7994f6af42fe6
lg0leg <lg.oleg1067@gmail.com>
2024-12-02 20:03:35 +0300

    feat: add  "no rated films found" message

--- movie-search-app/src/pages/rated-movies/rated-movies.js
+++ movie-search-app/src/pages/rated-movies/rated-movies.js
@@ -1,5 +1,29 @@
+import { Button, Center, Flex } from '@mantine/core';
 import React from 'react';
+import norated from '../../assets/svg/loading.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function RatedMovies() {
-  return <div>RatedMovies</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Center h="100vh">
+      <Flex gap="16" justify="center" align="center" direction="column">
+        <img src={norated} alt="You haven't rated any films yet" />
+        <h2>You haven't rated any films yet</h2>
+        <Button
+          variant="filled"
+          color="#9854f6"
+          size="md"
+          radius="md"
+          onClick={() => {
+            navigate('/movies');
+          }}
+        >
+          Find movies
+        </Button>
+      </Flex>
+    </Center>
+  );
+  // <div>RatedMovies</div>;
 }
