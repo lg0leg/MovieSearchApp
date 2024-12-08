a5686b589df123354ee4bdd5891213e3727342db
lg0leg <lg.oleg1067@gmail.com>
2024-12-08 20:46:05 +0300

    feat: add search by favorites

--- movie-search-app/src/pages/rated-movies/rated-movies.js
+++ movie-search-app/src/pages/rated-movies/rated-movies.js
@@ -1,6 +1,7 @@
-import { Button, Center, Container, Flex, Space, Title } from '@mantine/core';
-import React, { useContext } from 'react';
+import { Button, Center, Container, Flex, Space, TextInput, Title } from '@mantine/core';
+import React, { useContext, useState } from 'react';
 import norated from '../../assets/svg/loading.svg';
+import search from '../../assets/png/search.png';
 import { useNavigate } from 'react-router-dom';
 import { FavContext } from '../../state/state';
 import MovieCard from '../../components/movie-card/movie-card';
@@ -11,6 +12,14 @@ export default function RatedMovies() {
   const navigate = useNavigate();
 
   const genresLS = JSON.parse(localStorage.getItem('genresList'));
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchHandler = (event) => {
+    setSearchQuery(event.currentTarget.value);
+  };
+
+  const searchFilter = (item) => item.original_title.toLowerCase().includes(searchQuery.toLowerCase());
 
   return favContext.favState.favoritesId.length === 0 ? (
     <Center h="100vh">
@@ -32,12 +41,24 @@ export default function RatedMovies() {
     </Center>
   ) : (
     <Container fluid style={{ width: 1160, paddingLeft: 90, paddingRight: 90, paddingTop: 40, margin: 0 }}>
-      <Title order={1}>Rated movies</Title>
+      <Flex justify={'space-between'} align={'center'}>
+        <Title order={1}>Rated movies</Title>
+        <TextInput
+          className="search-input"
+          leftSectionPointerEvents="none"
+          leftSection={<img src={search} alt="search icon" />}
+          value={searchQuery}
+          onChange={searchHandler}
+          placeholder="Search movie title"
+          size="lg"
+          radius="md"
+        />
+      </Flex>
       <Space h="40" />
       <div className="favorites-container">
-        {favContext.favState.favoritesInfo.map((item) => (
-          <MovieCard key={item.id} info={item} genres={genresLS}></MovieCard>
-        ))}
+        {searchQuery === ''
+          ? favContext.favState.favoritesInfo.map((item) => <MovieCard key={item.id} info={item} genres={genresLS}></MovieCard>)
+          : favContext.favState.favoritesInfo.filter(searchFilter).map((item) => <MovieCard key={item.id} info={item} genres={genresLS}></MovieCard>)}
       </div>
     </Container>
   );
